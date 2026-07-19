c171e486c1ac31994d7c1481ce01e4ba2bf7abb0
Amanda Karunathilaka <amandakarunathilaka111@gmail.com>
2026-07-19 11:23:16 +0530

    Changes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,6 @@ import ProtectedRoute from './components/common/ProtectedRoute.jsx';
 import Layout         from './components/layout/Layout.jsx';
 import Home           from './pages/Home.jsx';
 import Login          from './pages/auth/Login.jsx';
-import Register       from './pages/auth/Register.jsx';
-import SelectRole     from './pages/auth/SelectRole.jsx';
 import Search         from './pages/Search.jsx';
 import NotFound       from './pages/NotFound.jsx';
 import DriverDashboard    from './pages/driver/DriverDashboard.jsx';
@@ -26,7 +24,6 @@ function App() {
           <Route path="/login"       element={<Login />} />
           <Route path="/register"    element={<Login />} />
           <Route path="/search"      element={<Search />} />
-          <Route path="/select-role" element={<SelectRole />} />
           <Route path="/rides/:id" element={<RideDetails  />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
 
